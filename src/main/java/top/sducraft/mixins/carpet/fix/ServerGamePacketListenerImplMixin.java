57cb60853249ba4acdd488ef7ac0eae3cea6854c
xhbsh <qq260342855@gmail.com>
2025-09-30 08:36:55 +0800

    bug fix 修复开启antiCheatDisabled仍然会出现xxx moved too quickly 的问题

---
+++ src/main/java/top/sducraft/mixins/carpet/fix/ServerGamePacketListenerImplMixin.java
@@ -0,0 +1,22 @@
+package top.sducraft.mixins.carpet.fix;
+
+import carpet.CarpetSettings;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public abstract class ServerGamePacketListenerImplMixin {
+
+    @Shadow
+    protected abstract boolean shouldCheckPlayerMovement(boolean bl);
+
+    @Redirect(method = "handleMovePlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;shouldCheckPlayerMovement(Z)Z"))
+    private boolean shouldCheckPlayerMovement1(ServerGamePacketListenerImpl instance, boolean bl) {
+        return CarpetSettings.antiCheatDisabled && this.shouldCheckPlayerMovement(bl);
+    }
+
+}
