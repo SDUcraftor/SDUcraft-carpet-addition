d72e546efe9ab05c6dd5aaac2b0f2ce14e8869d2
xhbsh <qq260342855@gmail.com>
2025-10-18 00:02:56 +0800

    防止标记方块实体被保存

---
+++ src/main/java/top/sducraft/mixins/rule/finditem/DisableVisualizerEntitySave.java
@@ -0,0 +1,23 @@
+package top.sducraft.mixins.rule.finditem;
+
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.Set;
+
+@Mixin(Entity.class)
+public abstract class DisableVisualizerEntitySave {
+    @Shadow
+    public abstract Set<String> getTags();
+
+    @Inject(method = "saveAsPassenger", at = @At("HEAD"), cancellable = true)
+    public void saveAsPassenger(CallbackInfoReturnable<Boolean> cir) {
+        Set<String> tags = this.getTags();
+        if (tags.contains("finditem_highlight")) {
+            cir.setReturnValue(false);
+        }
+    }
+}
